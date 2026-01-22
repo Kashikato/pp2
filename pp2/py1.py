17de9ad0d0dabca50fe1db7e43c9bc80975563c6
Kashikato <anuarmun2007@gmail.com>
2026-01-22 10:48:52 +0500

    random

--- pp2/py1.py
+++ pp2/py1.py
@@ -1,1 +1,9 @@
+import random
 print("hello")
+random_number = random.randint(1, 10)
+
+print(random_number)
+if random_number > 5:
+    print("Greater than 5")
+else:
+    print("Less than or equal to 5")
