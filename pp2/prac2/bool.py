5b9b943e741077b7a786496c57a3949e063d68d0
Kashikato <anuarmun2007@gmail.com>
2026-01-29 11:34:37 +0500

    bool values, boolean as comparison results, boolean operators

---
+++ pp2/prac2/bool.py
@@ -0,0 +1,46 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print(bool("Hello"))
+print(bool(15))
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+#bool operators
+x=5
+y=10
+x == y
+x != y 
+x > y
+x < y
+x >= y
+x <= y
+x = 5
+y = 3
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#boolean as comparison results
+x < 5 and  x < 10
+x < 5 or x < 4
+print(x < 5 and  x < 10)
+not(x < 5 and x < 10)
