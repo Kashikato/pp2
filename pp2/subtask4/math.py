db8b8f2d031f98b55510753d4cab64b57843ad6b
Kashikato <anuarmun2007@gmail.com>
2026-02-19 00:29:01 +0500

    math and random operations

---
+++ pp2/subtask4/math.py
@@ -0,0 +1,21 @@
+# import math
+# n=int(input())
+# ntopi=n/180*math.pi
+# print(round(ntopi,6))
+
+
+# h=int(input())
+# a=int(input())
+# b=int(input())
+# print((a+b)/2*h)
+
+
+# import math
+# n=int(input())
+# l=int(input())
+# print((n*l**2)/4*(1/math.tan(math.pi/n)))
+
+
+b=int(input())
+h=int(input())
+print(b*h)
