63279827a6712d3d2c97623dd56f180ab0978e3a
Kashikato <anuarmun2007@gmail.com>
2026-02-18 23:56:04 +0500

    date and time operations

---
+++ pp2/subtask4/dates.py
@@ -0,0 +1,22 @@
+# from datetime import datetime, timedelta
+# print(datetime.now()-timedelta(days=5))
+
+
+# from datetime import datetime, timedelta, date
+# d=date.today()
+# print(d-timedelta(days=1))
+# print(d)
+# print(d+timedelta(days=1))
+
+
+# import datetime
+# d1 = datetime.datetime.now()
+# d2 = d1.replace(microsecond=0)
+# print("Datetime without microseconds:", d2)
+
+
+# import datetime
+# d1 = datetime.datetime(2020, 5, 17)
+# d2 = datetime.datetime.now()
+# d3 = d2-d1
+# print(d3.days*24*3600+d3.seconds)
